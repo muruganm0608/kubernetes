3f1fb23f1d41b57b376c0780483e7dfc086d3c40
root <root@manager.prathiksha.com>
2021-08-07 20:55:44 +0200

    updated

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,6 +21,7 @@ echo "[Task- 4] : Sleeping for 5sec"
 sleep 5
 echo "[Task- 5] : Installing kubelet & kubeadm"
 cgroups_driver=`docker info 2>/dev/null | grep -i cgroup | awk -F':' '{print $2}' | xargs`
+yum install net-tools -y
 yum install kubeadm-1.22* kubectl-1.22* -y
 sed -i '/swap/d' /etc/fstab
 swapoff -a
@@ -29,8 +30,9 @@ setenforce 0
 iptables -F
 iptables-save
 systemctl enable --now kubelet.service
+ipaddress=`ifconfig eth1 2>/dev/null | grep "inet " | awk '{print $2}' | xargs`
 echo "[Task- 6]: Adding cgroups_driver $cgroups_driver"
-sed -i 's/KUBELET_EXTRA_ARGS=.*/KUBELET_EXTRA_ARGS=--cgroup-driver='"$cgroups_driver"'/g' /etc/sysconfig/kubelet
+sed -i 's/KUBELET_EXTRA_ARGS=.*/KUBELET_EXTRA_ARGS=--cgroup-driver='"$cgroups_driver"' --node-ip='"$ipaddress"'/g' /etc/sysconfig/kubelet
 systemctl enable --now kubelet.service
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl restart sshd
